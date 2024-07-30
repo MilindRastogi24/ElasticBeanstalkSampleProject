c50eb7c457129d3519ab89a10b2a7366c7fa2890
Milind-Rastogi2400 <milind.rastogi@nagarro.com>
2024-07-30 12:02:10 +0530

    added app files

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, jsonify, render_template
+import psycopg2
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="database-1.crau20ig249e.ap-south-1.rds.amazonaws.com",
+        database="postgres",
+        user="postgres",
+        password="password",
+        port='5432'
+    )
+    return conn
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/create_table')
+def create_table():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users_table (
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(80) ,
+            email VARCHAR(120) 
+        );
+    ''')
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return 'Table created successfully.'
+
+@app.route('/insert_user')
+def insert_user():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO users_table (username, email)
+        VALUES (%s, %s);
+    ''', ('testuser', 'testuser@example.com'))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return 'User inserted successfully.'
+
+@app.route('/fetch_users')
+def fetch_users():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT id, username, email FROM users_table;')
+    users = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    users_list = []
+    for user in users:
+        users_list.append({
+            'id': user[0],
+            'username': user[1],
+            'email': user[2]
+        })
+
+    return jsonify(users_list)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
