1b81734aa35f2a1bb3148cc0eb7473383ff9c207
Milind-Rastogi2400 <milind.rastogi@nagarro.com>
2024-07-31 09:39:30 +0530

    used secret manager

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
 from flask import Flask, jsonify, render_template
 import psycopg2
 
+import boto3
+from botocore.exceptions import ClientError
+
+def get_secret():
+
+    secret_name = "rds_secret"
+    region_name = "ap-south-1"
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    print(secret)
+
+    # Your code goes here.
+get_secret()
+
 app = Flask(__name__)
 
 def get_db_connection():
